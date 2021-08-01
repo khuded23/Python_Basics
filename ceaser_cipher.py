ed62f90da763ba3e43c742e75dfaa5f9a4dab636
Kiran Huded <Kiran.Huded@logmein.com>
2021-08-01 19:05:26 +0530

    ceaser-complete

--- ceaser_cipher.py
+++ ceaser_cipher.py
@@ -3,22 +3,22 @@ alphanum = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7, 'i':
             'z': 25}
 
 
-def encipher(alpha):
+def encipher(alpha,kw):
     ciphertext = str()
     for word in alpha:
         num = alphanum[word]
-        num +=3
+        num += kw
         for newword,value in alphanum.items():
             if num == value:
                 ciphertext+=newword
     print(ciphertext.upper())
 
 
-def decipher(alpha):
+def decipher(alpha,kw):
     ciphertext = str()
     for word in alpha:
         num = alphanum[word]
-        num -=3
+        num -= kw
         for newword,value in alphanum.items():
             if num == value:
                 ciphertext+=newword
@@ -29,11 +29,17 @@ what = input("do you want to encipher or decipher, enter 1 for encipher , anykey
 
 if what  == '1':
     en_word = (input("Enter a word to be enciphered: ").lower()).strip(" ")
-    encipher(en_word)
+    offset = input("Enter offset value(default =3,ceaser):")
+    if offset =='':
+        offset = 3
+    encipher(en_word,int(offset))
 else:
     dechiper = (input("Enter a word to be de-ciphered: ").lower()).strip(" ")
-    decipher(dechiper)
+    offset = int(input("Enter offset value(default =3,ceaser):"))
+    if offset =='':
+        offset = 3
+    decipher(dechiper,offset)
 
 
 
-
+#make ceaser KW
